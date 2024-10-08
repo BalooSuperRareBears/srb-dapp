9fbba94149188adaa6c368c43a80c1ccf039fa50
BalooSuperRareBears <105300746+BalooSuperRareBears@users.noreply.github.com>
2024-10-08 17:48:40 +0300

    Add files via upload

--- src/localConstants/routes/routeNames.enums.ts
+++ src/localConstants/routes/routeNames.enums.ts
@@ -2,5 +2,7 @@ export enum RouteNamesEnum {
   home = '/',
   dashboard = '/dashboard',
   unlock = '/unlock',
-  disclaimer = '/disclaimer'
+  disclaimer = '/disclaimer',
+  claimHype = '/claim-hype',
+  ais = '/ais'
 }
